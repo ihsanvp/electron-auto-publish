7510200d0ac393e2f9922258e7eb116995078409
Mohammed Ihsan <ihsanvp2738@gmail.com>
2022-01-09 13:34:07 +0530

    added check for updates ui

--- src/preload.ts
+++ src/preload.ts
@@ -2,6 +2,7 @@ import { ipcRenderer, contextBridge } from "electron";
 
 const appMethods = {
   getVersion: async () => ipcRenderer.invoke("app-version"),
+  checkForUpdates: async () => ipcRenderer.invoke("check-for-app-updates"),
 };
 
 contextBridge.exposeInMainWorld("app", appMethods);
