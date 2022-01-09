3d1dbff8942587a5d9e1670121dccfa1649321f2
Mohammed Ihsan <ihsanvp2738@gmail.com>
2022-01-09 14:35:39 +0530

    fix: feed url

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,7 @@ if (require("electron-squirrel-startup")) {
 }
 
 if (!electronIsDev) {
-  const server =
-    "https://electron-auto-publish-update-server-pi2uqmf3v-ihsanvp.vercel.app";
+  const server = "https://electron-auto-publish-update-server.vercel.app";
   const url = `${server}/update/${process.platform}/${app.getVersion()}`;
 
   autoUpdater.setFeedURL({ url });
