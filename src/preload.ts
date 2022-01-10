b2dd99b060eabc43f0d4298cce0c994e581c37a4
Mohammed Ihsan <ihsanvp2738@gmail.com>
2022-01-10 19:04:01 +0530

    added electron-updater

--- src/preload.ts
+++ src/preload.ts
@@ -1,10 +1,17 @@
 import { ipcRenderer, contextBridge } from "electron";
 
+type AnyFunction = (...args: any[]) => void;
+
 const appMethods = {
   getVersion: async () => ipcRenderer.invoke("app-version"),
   checkForUpdates: async () => ipcRenderer.invoke("check-for-app-updates"),
   onUpdateAvailable: (callback: VoidFunction) => {
     ipcRenderer.on("app-update-available", () => callback());
+  },
+  onUpdateDownload: (callback: AnyFunction) => {
+    ipcRenderer.on("app-download-progress", (...args) => {
+      callback(args);
+    });
   },
 };
 
